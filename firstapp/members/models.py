b9c1cf6b2d6d825212f70387715810bb3e4a54e4
Prabesh Gupta <prabeshtech232@gmail.com>
2024-06-23 11:07:26 -0700

    Django Admin

--- firstapp/members/models.py
+++ firstapp/members/models.py
@@ -5,3 +5,7 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+
+# String Representation Function: change the string representation of objects in Python
+# def __str__(self):
+  #   return f"{self.firstname} {self.lastname}"
